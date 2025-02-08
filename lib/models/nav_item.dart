c61e344fb12c4df838d426d709d91fca2693c9d1
SY <lunaslash9249@gmail.com>
2025-02-09 02:37:28 +0900

    day003 : 네비게이션 바 작성, Stateful Widget Life Cycle 복습

---
+++ lib/models/nav_item.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class NavItem {
+  final String id;
+  final String icon;
+  final String label;
+
+  NavItem({
+    required this.id,
+    required this.icon,
+    required this.label,
+  });
+}
+
+List<NavItem> navItems = [
+  NavItem(id: '0', label: '홈', icon: 'assets/icons/home.svg'),
+  NavItem(id: '1', label: '추천', icon: 'assets/icons/star.svg'),
+  NavItem(id: '2', label: '카테고리', icon: 'assets/icons/square.svg'),
+  NavItem(id: '3', label: '검색', icon: 'assets/icons/loupe.svg'),
+  NavItem(id: '4', label: '마이컬리', icon: 'assets/icons/user.svg'),
+];
--- lib/screens/main_screens.dart
+++ lib/screens/main_screens.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:market_curly_ui/constants.dart';
+import 'package:market_curly_ui/models/nav_item.dart';
 import 'package:market_curly_ui/screens/category/category_screen.dart';
 import 'package:market_curly_ui/screens/home/home_screen.dart';
 import 'package:market_curly_ui/screens/my_kurly/my_kurly_screen.dart';
@@ -14,19 +17,66 @@ class MainScreens extends StatefulWidget {
 }
 
 class _MainScreensState extends State<MainScreens> {
-  final int _selectedIndex = 0;
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: IndexedStack(
-      index: _selectedIndex,
-      children: [
-        HomeScreen(),
-        RecommendScreen(),
-        CategoryScreen(),
-        MyKurlyScreen(),
-      ],
-    ));
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          HomeScreen(),
+          RecommendScreen(),
+          CategoryScreen(),
+          MyKurlyScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.purple,
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.white,
+        onTap: _onItemTapped,
+        items: List.generate(
+            navItems.length,
+            (index) => _buildBottomNavigationBarItem(
+                  icon: navItems[index].icon,
+                  label: navItems[index].label,
+                  isActive: navItems[index].id == _selectedIndex.toString()
+                      ? true
+                      : false,
+                )),
+      ),
+    );
   }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
+
+_buildBottomNavigationBarItem(
+    {String? icon,
+    String? label,
+    bool isActive = false,
+    GestureTapCallback? press}) {
+  return BottomNavigationBarItem(
+      icon: SizedBox(
+          width: 38,
+          height: 38,
+          child: IconButton(
+              onPressed: press,
+              icon: SvgPicture.asset(
+                icon ?? "assets/icons/star.svg",
+                colorFilter: ColorFilter.mode(
+                  isActive ? kPrimaryColor : Colors.black,
+                  BlendMode.srcIn,
+                ),
+                width: 24,
+                height: 24,
+              ))),
+      label: label);
 }
