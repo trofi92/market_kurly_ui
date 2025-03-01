dab939d66c9d9a6104bf12c55fc3a5ee9b9140ff
SY <lunaslash9249@gmail.com>
2025-03-01 16:56:31 +0900

    day015: 검색기능 작업중, 개체 상태 FocusNode로 가져와서 이벤트 달아주기

---
+++ lib/screens/search/components/default_search_field.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:market_curly_ui/constants.dart';
+
+class DefaultSearchField extends StatefulWidget {
+  const DefaultSearchField({super.key});
+
+  @override
+  State<DefaultSearchField> createState() => _DefaultSearchFieldState();
+}
+
+class _DefaultSearchFieldState extends State<DefaultSearchField> {
+  final FocusNode _focusNode = FocusNode();
+  bool isFocus = false;
+
+  @override
+  void initState() {
+    super.dispose();
+    SystemChannels.textInput.invokeMethod('TextInput.show');
+    _focusNode.addListener(() => _onFocusChange());
+  }
+
+  void _onFocusChange() {
+    setState(() {
+      isFocus = !isFocus;
+    });
+  }
+
+  void _unFocus() {
+    _focusNode.unfocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            focusNode: _focusNode,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              filled: true,
+              fillColor: Colors.grey[200],
+              hintText: "검색어를 입력해주세요",
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ),
+        // ignore: sized_box_for_whitespace
+        Container(
+            width: isFocus ? 50 : 0,
+            child: isFocus
+                ? Center(
+                    child: GestureDetector(
+                        onTap: _unFocus,
+                        child: Text(
+                          "취소",
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        )),
+                  )
+                : SizedBox()),
+      ],
+    );
+  }
+}
--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:market_curly_ui/constants.dart';
+import 'package:market_curly_ui/screens/components/custom_actions.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -6,7 +8,19 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Search Screen")),
+      appBar: AppBar(
+        title: Text("검색"),
+        actions: [CustomActions()],
+        automaticallyImplyLeading: false,
+      ),
+      body: ListView(children: [
+        //
+        //DefaultSearchField
+        const SizedBox(height: 20),
+        Text("인기 검색어", style: TextStyle(color: kTextColor))
+        //
+        // ListView.seaprated
+      ]),
     );
   }
 }
